8fc907bf555e7e8027f7a1cde5a496e3e8fd1f59
LeRoy-M <lmwasela@gmail.com>
2022-10-18 12:34:21 -0700

    8. Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:.\w*\]/).join
-puts ARGV[0].scan(/\[to:.\w*\]/).join
-puts ARGV[0].scan(/\[flags:.\S*\]/).join
+puts ARGV[0].scan(/(?<=from:|to:|flags:)[^\]]*/).join(", ")
