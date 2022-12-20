1897dd2e6fdf2b537f89d7e0c42d9b6e16858051
LeRoy-M <lmwasela@gmail.com>
2022-12-20 10:09:39 -0800

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Configures the SSH client configuration file
-file { 'PasswordAuthentication':
+file_line { 'PasswordAuthentication':
   path    => '/etc/ssh/ssh_config',
   line    => '	PasswordAuthentication no',
   replace => true,
 }
 
-file { 'IdentityFile':
+file_line { 'IdentityFile':
   path    => '/etc/ssh/ssh_config',
   line    => '	IdentityFile ~/.ssh/school',
   replace => true,
