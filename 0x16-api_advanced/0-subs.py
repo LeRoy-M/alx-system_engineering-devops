b64c5f26abe1cfe3cf31889cd5beeb38736ab7b0
LeRoy-M <lmwasela@gmail.com>
2023-02-01 12:16:06 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,13 @@ Module 0-subs
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Function that returns number of subscribers via Reddit API"""
     header = {"User-Agent": "Chrome/66.0.3359.139 Mobile Safari/537.36"}
     url = "https://api.reddit.com/r/{}/about".format(subreddit)
     response = requests.get(url, headers=header)
-    
+
     if response.status_code == 200:
         subs = response.json()["data"]["subscribers"]
     else:
