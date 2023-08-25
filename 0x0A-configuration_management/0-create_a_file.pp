29efefdd60ad7d7ab178f3d6f5dccdfc393a1e0f
LeRoy-M <lmwasela@gmail.com>
2023-08-25 12:19:53 +0300

    0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Manifest that creates a file
 file { 'school':
-  ensure  => file
-  path    => '/tmp/'
-  content => 'I love Puppet'
-  mode    => '0744'
-  owner   => www-data
-  group   => www-data
+  ensure  => file,
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => www-data,
+  group   => www-data,
 }
