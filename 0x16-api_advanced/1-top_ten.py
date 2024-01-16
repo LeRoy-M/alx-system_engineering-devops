69c5aaecc8b8bacd7fd9335a6060121a9939a53c
LeRoy-M <lmwasela@gmail.com>
2024-01-16 17:31:02 +0300

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,17 +10,16 @@ def top_ten(subreddit):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 " +
                "Safari/537.36"}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
     try:
-        for _ in range(10):
-            print(response.json().get("data").get("children")[_].
-                  get("data").get("title"))
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9"
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        top_titles = response.json().get("data").get("children")
+
+        for _ in top_titles:
+            print(_.get("data").get("title"))
     except Exception:
         print(None)
 
 
 if __name__ == "__main__":
-    subreddit = "programming"
     top_ten(subreddit)
