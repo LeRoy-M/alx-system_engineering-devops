ca9a32675db0cc8164dc523a81bd7b9651ef121d
LeRoy-M <lmwasela@gmail.com>
2024-01-28 16:17:57 +0300

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Manifest that tests how well the web server setup works
 exec { 'skynotlimit':
-  command => 'sudo sed -i "s/15/10000" /etc/default/nginx && sudo service nginx restart',
+  command => 'sudo sed -i "s/15/1000" /etc/default/nginx && sudo service nginx restart',
   path    => ['/usr/local/bin/', '/usr/local/sbin/', '/usr/bin/', '/usr/sbin/', '/bin/'],
 }
