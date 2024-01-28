8a4a994c5cc20f113984442a69b23f598fd10f70
LeRoy-M <lmwasela@gmail.com>
2024-01-28 16:42:26 +0300

    1. User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
 # Manifest that changes the OS configuration to make it possible to login with `holberton` user and open a file
-exec { 'userlimit':
-  command => 'sudo sed -i "/holberton/s/5/50000" /etc/security/limits.conf',
+exec { 'userlimitsoft':
+  command => 'sudo sed -i "/holberton soft/s/4/50000" /etc/security/limits.conf',
+  path    => ['/usr/local/bin/', '/usr/local/sbin/', '/usr/bin/', '/usr/sbin/', '/bin/', '/sbin/'],
+}
+
+exec { 'userlimithard':
+  command => 'sudo sed -i "/holberton hard/s/5/50000" /etc/security/limits.conf',
   path    => ['/usr/local/bin/', '/usr/local/sbin/', '/usr/bin/', '/usr/sbin/', '/bin/', '/sbin/'],
 }
