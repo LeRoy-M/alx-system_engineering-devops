ea7c3833ac7d9d748da97d9cd47178e30f8aaa37
LeRoy-M <lmwasela@gmail.com>
2024-03-05 23:00:13 +0300

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Manifest that automates server 500 error fix
 server 'fix-500_server_error' {
   command => 'sudo pgrep apache2 & export PID=$! & sudo strace -p $PID -f -o strace.log',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  path   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
