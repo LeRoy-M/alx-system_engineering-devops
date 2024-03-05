a620441a8c4b8b58e48154aefc41ca038cf040d9
LeRoy-M <lmwasela@gmail.com>
2024-03-05 22:49:45 +0300

    0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that queries a Reddit API and returns the number of
+subscribers for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that returns number of subcribers of given subreddit"""
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+               "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 " +
+               "Safari/537.36"}
+    try:
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        response = requests.get(url, headers=headers)
+
+        return response.json().get("data").get("subscribers")
+    except Exception:
+        return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(subreddit)
